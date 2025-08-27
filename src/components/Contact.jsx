ad1ca5287315bf1992042b8a1a8e32bde91aed89
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2025-08-27 16:59:00 +0200

    update
    actualizo CV

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ export default function Contact() {
 
         <div className={styles.contactInfo}>
           <p>carlosriberadonet@gmail.com</p>
-          <p><a href="tel:638662620">638662620</a></p>
+          <p><a href="tel:643814810">643814810</a></p>
         </div>
 
         <button type="submit">Enviar</button>
