5ac406b2d41d58b77be8b201535839b55659574a
Carlos Ribera Donet <carlosriberadonet@gmail.com>
2026-04-19 17:48:46 +0200

    re-build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Cambiamos la ruta absoluta por una relativa para evitar el error de resolución
-  base: './', 
+  base: '/Carlos-Ribera/',
   plugins: [react()],
 });
